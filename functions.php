dc9d6c327525e5d053a0f98613b4767fadeb35b8
Joe Rong <jmrong@gmail.com>
2015-11-15 00:17:10 -0500

    add pullquote shortcode and register shortcode UI for pullquote shortcode

--- functions.php
+++ functions.php
@@ -14,3 +14,58 @@ function twentyfifteen_parent_theme_enqueue_styles() {
     );
 
 }
+
+add_action( 'init', function() {
+    /**
+     * Register your shortcode as you would normally.
+     * This is a simple example for a pullquote with a citation.
+     */
+    add_shortcode( 'pullquote', function( $attr, $content = '' ) {
+        $attr = wp_parse_args( $attr, array(
+            'source' => ''
+        ) );
+        ob_start();
+        ?>
+
+        <section class="pullquote">
+            <?php echo esc_html( $content ); ?><br/>
+            <?php if ( ! empty( $attr['source'] ) ) : ?>
+                <cite><em><?php echo esc_html( $attr['source'] ); ?></em></cite>
+            <?php endif; ?>
+        </section>
+
+        <?php
+        return ob_get_clean();
+    } );
+    /**
+     * Register a UI for the Shortcode.
+     * Pass the shortcode tag (string)
+     * and an array or args.
+     */
+    shortcode_ui_register_for_shortcode(
+        'pullquote',
+        array(
+            // Display label. String. Required.
+            'label' => 'Pullquote',
+            // Icon/image for shortcode. Optional. src or dashicons-$icon. Defaults to carrot.
+            'listItemImage' => 'dashicons-editor-quote',
+            // Available shortcode attributes and default values. Required. Array.
+            // Attribute model expects 'attr', 'type' and 'label'
+            // Supported field types: text, checkbox, textarea, radio, select, email, url, number, and date.
+            'attrs' => array(
+                array(
+                    'label' => 'Quote',
+                    'attr'  => 'content',
+                    'type'  => 'textarea',
+                ),
+                array(
+                    'label'       => 'Cite',
+                    'attr'        => 'source',
+                    'type'        => 'text',
+                    'placeholder' => 'Firstname Lastname',
+                    'description' => 'Optional',
+                ),
+            ),
+        )
+    );
+} );
